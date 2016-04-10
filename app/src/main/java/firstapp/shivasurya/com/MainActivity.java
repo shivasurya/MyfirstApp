b6e5ee131c74bdc718ae132a69c2f9906b446f37
s.shivasurya <s.shivasurya@gmail.com>
2016-04-10 09:50:01 +0530

    feat : pizza corner app
    pizza corner app as my first app for Udacity project.
    contains
     1) Business name
     2) Business Address
     3) Business Phone
     4) Business Description
     5) Business Cover Photo

---
+++ app/src/main/java/firstapp/shivasurya/com/MainActivity.java
@@ -0,0 +1,27 @@
+package firstapp.shivasurya.com;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button mContactNow = (Button) findViewById(R.id.contactnow);
+        //null check to ensure that button has reference and not null
+        if (mContactNow != null) {
+            mContactNow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "08044112233"));
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+}
